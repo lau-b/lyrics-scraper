78c4490657dbcdd6833d3d80c2ae02e52abab87a
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-04 15:48:57 +0100

    removes pandas from scraping logic
    I am still not happy with the way I save lists to files

--- src/scraper.py
+++ src/scraper.py
@@ -2,15 +2,12 @@
 import requests
 import time
 import os
-import pandas as pd
 import utils
 import re
 from bs4 import BeautifulSoup as bs4
 
 
-
 def scrape_artist_overview(artist):
-    # print(f'downloading: {artist}')
     r = requests.get(f'https://www.lyrics.com/artist/{artist}')
 
     web_page = open(f'{utils.get_project_root()}/data/raw/{utils.clean_artist(artist)}.html', 'w')
@@ -26,29 +23,28 @@ def scrape_lyrics_page(artist, song, url):
     web_page.close()
     time.sleep(0.3)
 
+
 def extract_lyrics(filepath):
     try:
         with open(filepath, 'r') as file:
             soup = bs4(file, 'html.parser')
             return re.sub(r'\n', ' ', soup.body.pre.text)
-    except FileNotFoundError:
-        print(f'could not find {filepath}')
+    except (AttributeError, FileNotFoundError) as e:
+        # print(f'could not find {filepath}')
+        pass
 
 
+def find_songs_and_url(artist):
+    path = (f'{utils.get_project_root()}/data/raw/{artist}.html')
+    with open(path, 'r') as file:
+        soup = bs4(file, 'html.parser')
 
-
-def find_songs_and_urls(artist):
-
-    with open(f'{utils.get_project_root()}/data/raw/{artist}.html') as fp:
-        soup = bs4(fp, 'html.parser')
-
-    print(f'finding songs for {artist}')
-    songs = []
-    musician = soup.body.find('h1', 'artist').string
+    musician = utils.clean_string(soup.body.find('h1', 'artist').string)
     table_data = soup.body.find_all('td', 'tal qx')
 
     for data in table_data:
-        songs.append({'artist': utils.clean_string(musician),
-                      'song': utils.clean_string(data.string),
-                      'url': data.a['href']})
-    return songs
+        song = utils.clean_string(data.string)
+        url = data.a['href']
+        overview_file = open(f'{utils.get_project_root()}/data/processed/overview.csv', 'a')
+        overview_file.write(f'{musician}|{song}|{url}\n')
+        overview_file.close
