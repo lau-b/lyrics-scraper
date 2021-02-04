e086ffb763583142a8944c18b5b6d532495db8af
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-04 15:47:03 +0100

    extracting lyrics from html files finished

--- src/extract_lyrics.py
+++ src/extract_lyrics.py
@@ -5,17 +5,14 @@ from tqdm import tqdm
 song_list = []
 with open(f'{utils.get_project_root()}/data/processed/overview.csv', 'r') as file:
     for line in file:
-        song_list.append((line.split(',')))
+        song_list.append((line.split('|')))
 
 
 for entry in tqdm(song_list):
-    read_path = (f'{utils.get_project_root()}/data/raw/artists/{entry[1]}/{entry[2]}.html')
+    read_path = (f'{utils.get_project_root()}/data/raw/artists/{entry[0]}/{entry[1]}.html')
     write_path = f'{utils.get_project_root()}/data/processed/list-of-lyrics.txt'
-
     lyrics = scraper.extract_lyrics(read_path)
 
     file = open(write_path, 'a')
-    file.write(f'{entry[1]}|{lyrics}\n')
+    file.write(f'{entry[0]}|{entry[1]}|{lyrics}\n')
     file.close()
-
-print('Done')
