f4bcc2bfd595c63db9f8077ff52276b62408ebb9
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-04 15:43:41 +0100

    utils finished

--- src/utils.py
+++ src/utils.py
@@ -11,19 +11,20 @@ def create_folders(artist):
     try:
         os.makedirs(f'{get_project_root()}/data/raw/artists/{artist}')
     except FileExistsError:
+        # maybe include loggin here later
         pass
-        # print(f'{artist}: Directory already exsists.')
 
     try:
         os.makedirs(f'{get_project_root()}/data/processed/artists/{artist}')
     except FileExistsError:
+        # maybe include loggin here later
         pass
-        # print(f'{artist}: Directory already exsists.')
 
 
 def clean_string(string):
     string = re.sub(r'\s+', '-', re.sub(r'\.|,|\/', '', string)).lower()
     return string
+
 
 def clean_artist(artist):
     return re.sub(r'\/\d{3,}', '', artist).lower()
