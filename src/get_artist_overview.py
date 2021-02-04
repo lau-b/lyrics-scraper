4c0d8109e9f3e69f784443ebee0ebb7e12a27aeb
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-04 15:42:17 +0100

    iteration for get_artist finished

--- src/get_artist_overview.py
+++ src/get_artist_overview.py
@@ -1,41 +1,22 @@
-#! python3
-'''
-This file reads a list of artists and scrapes www.lyrics.com for a list of
-songs. It stores the artist name, song name and url to the lyrics page
-in a .csv file.
-'''
-
 import utils
-import os
-from pathlib import Path
 import scraper
-import pandas as pd
+import re
 from tqdm import tqdm
 
-# set absolute path
-PATH = Path(f'{utils.get_project_root()}/data/artists.csv')
+artist_list = []
 
-artist_list = pd.read_csv(PATH)
-df = pd.DataFrame({'artist': [],
-                   'song': [],
-                   'url': []})
+with open(f'{utils.get_project_root()}/data/artists.csv', 'r') as file:
+    for line in file:
+        artist_list.append(re.sub(r'\n', '', line))
 
-# creating missing folders to store lyric pages in
-for artist in tqdm(artist_list['Artist']):
-    # getting artist overview pages
+print('Step 1: Creating folder structure for every artist')
+for artist in tqdm(artist_list):
+    utils.create_folders(utils.clean_artist(artist))
+
+print('\nStep 2: Scraping lyrics.com\'s artist page')
+for artist in tqdm(artist_list):
     scraper.scrape_artist_overview(artist)
-    artist = utils.clean_artist(artist)
 
-    try:
-        os.makedirs(f'{utils.get_project_root()}/data/raw/artists/{artist}')
-    except FileExistsError:
-        print(f'{artist}: Directory already exsists.')
-
-
-
-    # extracting artist and song information and saving into a csv
-    df = df.append(scraper.find_songs_and_urls(artist))
-
-
-df.drop_duplicates(['artist', 'song'], inplace=True)
-df.to_csv(f'{utils.get_project_root()}/data/processed/overview.csv')
+print('\nStep 3: Extracting all songs and URLs from every artists page')
+for artist in tqdm(artist_list):
+    scraper.find_songs_and_url(utils.clean_artist(artist))
