5eb96a74efd77aac0fadbab6d5b79a7cacca77ad
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-02-05 15:06:55 +0100

    finished model and cli

--- src/predict_this.py
+++ src/predict_this.py
@@ -19,10 +19,17 @@ print('aaaand that\'s about it...')
 
 lyrics = input('Now let\'s start.. Give me some lyrics. Also, you can just leave by pressing \'x\': ')
 while lyrics != 'x':
-    answer = predictor.predict([lyrics])
-    print(f'Oh this is easy. It is {answer[0].capitalize()}, why do you even bother wasting my time?\n')
-    lyrics = input('Go, shoot me some difficult lyrics, please: ')
-    if lyrics == 'x':
-        print('OK THEN GET OFF! AND I REALLY KNOW THE OTHERS! I SWEAR! BYE!')
-    if lyrics == 'should i stay or should i go?':
-        print('\nHonestly I think you should go now...')
+    try:
+        answer = predictor.predict([lyrics])
+        print(f'Oh this is easy. It is {answer[0].capitalize()}, why do you even bother wasting my time?\n')
+        lyrics = input('Go, shoot me some difficult lyrics, please: ')
+        if lyrics == 'x':
+            print('OK THEN GET OFF! AND I REALLY KNOW THE OTHERS! I SWEAR! BYE!')
+        if lyrics == 'should i stay or should i go?':
+            print('\nHonestly I think you should go now...')
+            time.sleep(2)
+            print('Yes, you should go.')
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print('\n\nHaha\nI told you how can get out of here. This cheap trick wont work on me!')
+
